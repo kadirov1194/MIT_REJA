10687aaac69be38fe6f7695efa35bc70cdc61e4f
kadirov1194 <shoxrux.qodirov@gmail.com>
2023-09-18 12:37:45 +0900

    an

---
+++ Animals_Challenge/animals.js
@@ -0,0 +1,27 @@
+const animal_list = [
+  "fox",
+  "ant",
+  "bird",
+  "lion",
+  "wolf",
+  "deer",
+  "bear",
+  "frog",
+  "hen",
+  "mole",
+  "duck",
+  "goat",
+  "dog",
+  "cat",
+  "bat",
+  "cock",
+  "cow",
+];
+
+function findAnimals(txt) {
+  if (txt == ele.length) {
+  }
+}
+
+const javob = findAnimals(txt, "dgoat");
+console.log("Javob: ", javob);
